f42ea85b878ed2e5b6976adb17e0fed6ee42f694
Jerry <dmz1992@126.com>
2017-09-22 11:23:52 +0800

    修改包名

---
+++ app/src/main/java/com/jerry/utilityclass/MainActivity.java
@@ -0,0 +1,30 @@
+package com.jerry.utilityclass;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        testMethod();
+    }
+
+    private void testMethod() {
+
+
+    }
+
+    /**
+     * 跳转到ToastActivity
+     *
+     * @param v
+     */
+    public void toToastActivity(View v) {
+        startActivity(new Intent(this, ToastActivity.class));
+    }
+}
---
+++ app/src/main/java/com/jerry/utilityclass/tools/NetUtils_Simple.java
@@ -0,0 +1,85 @@
+package com.jerry.utilityclass.tools;
+
+import android.app.AlertDialog;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * 跟网络相关的工具类
+ * <p>
+ * Created by Jerry_D001 on 2016/8/10.
+ */
+public class NetUtils_Simple {
+    private NetUtils_Simple() {
+        /* cannot be instantiated */
+        throw new UnsupportedOperationException("cannot be instantiated");
+    }
+
+    /**
+     * 判断网络是否连接
+     *
+     * @param context
+     * @return
+     */
+    public static boolean isConnected(Context context) {
+
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (null != cm) {
+            NetworkInfo info = cm.getActiveNetworkInfo();
+            if (null != info && info.isConnected()) {
+                if (info.getState() == NetworkInfo.State.CONNECTED) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 判断是否是wifi连接
+     */
+    public static boolean isWifi(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (cm == null) {
+            return false;
+        }
+        return cm.getActiveNetworkInfo().getType() == ConnectivityManager.TYPE_WIFI;
+    }
+
+    /**
+     * 打开网络设置界面
+     */
+    public static void openNetSetting(final Context context) {
+        //提示对话框
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("网络设置提示").setMessage("网络连接不可用,是否进行设置?").setPositiveButton("设置", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // TODO Auto-generated method stub
+                Intent intent;
+                //判断手机系统的版本  如果API大于10(就是3.0或以上版本)
+                if (android.os.Build.VERSION.SDK_INT > 10) {
+                    intent = new Intent(android.provider.Settings.ACTION_WIRELESS_SETTINGS);
+                } else {
+                    intent = new Intent();
+                    ComponentName component = new ComponentName("com.android.settings", "com.android.settings.WirelessSettings");
+                    intent.setComponent(component);
+                    intent.setAction("android.intent.action.VIEW");
+                }
+                context.startActivity(intent);
+            }
+        }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        }).show();
+    }
+
+}
