36941ade5bf41a91ce18dbb1c16f1a63d02db109
Jerry <dmz1992@126.com>
2017-09-22 11:33:54 +0800

    添加判断对象是否为空工具类

---
+++ app/src/main/java/com/jerry/utilityclass/tools/EmptyUtil.java
@@ -0,0 +1,48 @@
+package com.jerry.utilityclass.tools;
+
+import java.util.List;
+
+/**
+ *
+ * 判断对象是否为空
+ *
+ * Created by Jerry on 2017/9/22.
+ */
+
+public class EmptyUtil {
+    /**
+     * 判断对象为空
+     *
+     * @param obj
+     *            对象名
+     * @return 是否为空
+     */
+    public static boolean isEmpty(Object obj)
+    {
+        if (obj == null)
+        {
+            return true;
+        }
+        if (obj instanceof List)
+        {
+            return ((List) obj).size() == 0;
+        }
+        if (obj instanceof String)
+        {
+            return ((String) obj).trim().equals("");
+        }
+        return false;
+    }
+
+    /**
+     * 判断对象不为空
+     *
+     * @param obj
+     *            对象名
+     * @return 是否不为空
+     */
+    public static boolean isNotEmpty(Object obj)
+    {
+        return !isEmpty(obj);
+    }
+}
